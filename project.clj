dc123a9968298c8edc68bdc04d962b8a9a6f718c
Alexander Solovyov <alexander@solovyov.net>
2019-01-28 21:56:35 +0200

    1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ua.kasta/clockwork "1.0.0"
+(defproject ua.kasta/clockwork "1.0.1"
   :description "Devtools library - server-side component"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
